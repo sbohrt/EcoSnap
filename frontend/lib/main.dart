9dbd6768f6c5bd2ff0ca0f166317ab71e0f4e3f2
Amado Krsul Portugal <132724295+amadokrsul@users.noreply.github.com>
2023-11-11 13:19:15 -0500

    Add camera functionality

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,20 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+  // can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      home: EcoSnap(
+        camera: firstCamera,
+      ),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class EcoSnap extends StatefulWidget {
+  const EcoSnap({
+    super.key,
+    required this.camera,
+  });
+
+  final CameraDescription camera;
+
+  @override
+  State<EcoSnap> createState() => _EcoSnapState();
+}
+
+class _EcoSnapState extends State<EcoSnap> {
+  late CameraController _controller;
+  late Future<void> _initializedControllerFuture;
+
+  @override
+  void initState() {
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.ultraHigh,
+    );
+
+    _initializedControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return Scaffold(
+      body: FutureBuilder<void>(
+          future: _initializedControllerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return CameraPreview(_controller);
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
     );
   }
 }
